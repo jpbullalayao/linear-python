68e1f7ef16b173ab37520833780d320eb8a240aa
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2024-12-19 19:50:24 -0800

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="linear-python",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=[
         "requests>=2.31.0",
